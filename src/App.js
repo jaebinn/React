f05fa1edfe4462ce4e459cccd8c82c1999bd3fc0
jaebinn <whiteclover129@gmail.com>
2024-06-10 21:36:24 +0900

    first React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MyComponent name="김사과"></MyComponent>
     </div>
   );
 }
